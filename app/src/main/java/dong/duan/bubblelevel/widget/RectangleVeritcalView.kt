cc4eceff96ded28d1d08105c9e585e087de09116
Dong0610 <bdong0610@gmail.com>
2023-08-23 19:42:44 +0700

    comment

--- app/src/main/java/dong/duan/bubblelevel/widget/RectangleVeritcalView.kt
+++ app/src/main/java/dong/duan/bubblelevel/widget/RectangleVeritcalView.kt
@@ -53,7 +53,6 @@ open class RectangleVeritcalView : View {
             resolveSize(h, heightMeasureSpec)
         )
     }
-
     override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
         super.onLayout(changed, left, top, right, bottom)
         val height = height
